7fa071e3e6adb2b9280f7a6197c15a77b7674296
Ashish <Ashish.Sharma@data-axle.com>
2022-09-18 13:24:59 +0530

    User Access Finalized

--- app/policies/team_policy.rb
+++ app/policies/team_policy.rb
@@ -7,11 +7,11 @@
     end
 
     def index?
-        @current_user.admin?
+        @current_user.admin? or @current_user.team_leader?
     end
 
     def show?
-        @current_user.admin?
+        @current_user.admin? or @current_user.team_leader?
     end
 
     def new?
